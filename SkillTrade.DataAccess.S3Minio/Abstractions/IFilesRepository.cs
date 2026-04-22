5f06d2c226221a3ef4fbe8cf2bec78f4e3d4ef24
sergxlove <sergxlove@mail.ru>
2026-04-22 21:25:12 +0600

    add s3 minio

---
+++ SkillTrade.DataAccess.S3Minio/Abstractions/IFilesRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace SkillTrade.DataAccess.S3Minio.Abstractions
+{
+    public interface IFilesRepository
+    {
+        Task CreateBucketIfNotExistsAsync(string bucketName, CancellationToken token);
+        Task<bool> DeleteAsync(string bucketName, string fileName, CancellationToken token);
+        Task<Stream?> DownloadFromNameAsync(string fileName, string bucketName, CancellationToken token);
+        Task<bool> ExistsObjectAsync(string bucketName, string fileName, CancellationToken token);
+        Task<string> UploadFileAsync(string bucketName, string fileName, Stream fileStream, CancellationToken token);
+    }
+}

50e7bb60f104bb0e1c0da989a77f643e28fe2f02
sergxlove <sergxlove@mail.ru>
2026-04-27 01:59:19 +0600

    update ui

--- SkillTrade.ContentAPI/Extensions/RegistrEndpoints.cs
+++ SkillTrade.ContentAPI/Extensions/RegistrEndpoints.cs
@@ -1,9 +1,12 @@
-﻿namespace SkillTrade.ContentAPI.Extensions
+﻿using SkillTrade.ContentAPI.Endpoints;
+
+namespace SkillTrade.ContentAPI.Extensions
 {
     public static class RegistrEndpoints
     {
         public static IEndpointRouteBuilder MapAllEndpoints(this IEndpointRouteBuilder app)
         {
+            app.MapPagesEndpoints();
 
             return app;
         }

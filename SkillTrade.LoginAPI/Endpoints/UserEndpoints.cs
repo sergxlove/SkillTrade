4ce2ed8c1a0488539ce602e35204f88acb427ba5
sergxlove <sergxlove@mail.ru>
2026-04-29 00:00:43 +0600

    refactoring pages, add new endpoints, add docker compose

---
+++ SkillTrade.LoginAPI/Endpoints/UserEndpoints.cs
@@ -0,0 +1,16 @@
+﻿namespace SkillTrade.LoginAPI.Endpoints
+{
+    public static class UserEndpoints
+    {
+        public static IEndpointRouteBuilder MapUserEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/api/login/user/balance", () =>
+            {
+
+            });
+
+
+            return app;
+        }
+    }
+}

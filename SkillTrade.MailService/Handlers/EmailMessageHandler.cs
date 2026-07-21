0dc331cffdf066332672d98124eb5d8fb7a83fa1
sergxlove <sergxlove@mail.ru>
2026-07-21 20:16:03 +0600

    add execute in worker service, add rabbitMQ in docker compose

---
+++ SkillTrade.MailService/Handlers/EmailMessageHandler.cs
@@ -0,0 +1,41 @@
+﻿using SkillTrade.Core.Models;
+using SkillTrade.MailService.Abstractions;
+using System.Text.Json;
+
+namespace SkillTrade.MailService.Handlers
+{
+    public class EmailMessageHandler
+    {
+        private readonly ISendMailService _mailService;
+        private JsonSerializerOptions _options = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        public EmailMessageHandler(ISendMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public async Task<string> HandleMessageAsync(string messageJson)
+        {
+            try
+            {
+                MailContent? mailContent = JsonSerializer.Deserialize<MailContent>(messageJson, _options);
+                if (mailContent == null)
+                {
+                    return "Error deserialize";
+                }
+                CancellationTokenSource cts = new(1000);
+                bool result = await _mailService.SendEmailAsync(mailContent, cts.Token);
+                if (result)
+                    return string.Empty;
+                else
+                    return "Error send in service";
+            }
+            catch
+            {
+                return "Error send in handler";
+            }
+        }
+    }
+}
---
+++ SkillTrade.MessageBroker.RabbitMQ/Abstractions/IProviderRabbitMQService.cs
@@ -0,0 +1,15 @@
+﻿using SkillTrade.MessageBroker.RabbitMQ.Models;
+using SkillTrade.MessageBroker.RabbitMQ.Services;
+
+namespace SkillTrade.MessageBroker.RabbitMQ.Abstractions
+{
+    public interface IProviderRabbitMQService
+    {
+        static abstract Task<ProviderRabbitMQService> CreateAsync();
+        void Dispose();
+        string GetQueueName(RoutingKeys routingKey);
+        Task InitializeQueuesAsync();
+        Task<bool> ReceiveMessageAsync(RoutingKeys routingKey, Func<string, Task<string>> messageHandler);
+        Task<bool> SendMessageAsync<T>(T message, RoutingKeys routingKey);
+    }
+}

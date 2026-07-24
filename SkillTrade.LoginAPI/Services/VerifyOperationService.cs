a90e102626594e99fb1ad6440e70ec1d554c1228
sergxlove <sergxlove@mail.ru>
2026-07-24 17:47:41 +0600

    add endpoint restore

---
+++ SkillTrade.LoginAPI/Services/VerifyOperationService.cs
@@ -0,0 +1,31 @@
+﻿using SkillTrade.Core.Models;
+using SkillTrade.DataAccess.Postgres.Abstractions;
+using SkillTrade.LoginAPI.Abstractions;
+
+namespace SkillTrade.LoginAPI.Services
+{
+    public class VerifyOperationService : IVerifyOperationService
+    {
+        private readonly IVerifyOperationsRepository _repository;
+        public VerifyOperationService(IVerifyOperationsRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<Guid> AddAsync(VerifyOperations oper, CancellationToken token)
+        {
+            return await _repository.AddAsync(oper, token);
+        }
+        public async Task<int> DeleteAsync(string email, CancellationToken token)
+        {
+            return await _repository.DeleteAsync(email, token);
+        }
+        public async Task<int> IncrementTryAsync(string email, CancellationToken token)
+        {
+            return await _repository.IncrementTryAsync(email, token);
+        }
+        public async Task<bool> VerifyAsync(VerifyOperations oper, int minutesValid, CancellationToken token)
+        {
+            return await _repository.VerifyAsync(oper, minutesValid, token);
+        }
+    }
+}
